442460d5e00a47c664f9c1ab874145b63dbad69a
vpompeudm <vinicius.pompeu.dm@gmail.com>
2025-04-08 14:35:49 -0300

    initial lazyvim config

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+      scratch = { enabled = false },
+      terminal = { enabled = false },
+      scroll = { enabled = false },
+    },
+  },
+}
