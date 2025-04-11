9919933f52f49fbb9fa412bebc79fbed99ea9cf5
vpompeudm <vinicius.pompeu.dm@gmail.com>
2025-04-11 18:19:23 -0300

    adding language support

---
+++ lua/plugins/terraform.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "terraform", "hcl" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        terraformls = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "tflint" } },
+  },
+  {
+    "ANGkeith/telescope-terraform-doc.nvim",
+    ft = { "terraform", "hcl" },
+    config = function()
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("terraform_doc")
+      end)
+    end,
+  },
+  {
+    "cappyzawa/telescope-terraform.nvim",
+    ft = { "terraform", "hcl" },
+    config = function()
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("terraform")
+      end)
+    end,
+  },
+}
